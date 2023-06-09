312ca90e7f2996fde394a4678d5365a66d4b7dc8
TashaTan1 <136006359+TashaTan1@users.noreply.github.com>
2023-06-09 18:56:22 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,133 @@
+/***************************************************************************************
+*    Title: puddle of stars
+*    Author: Sophia (fractal kitty)
+*    Date: n.d.
+*    Code version: 1.0
+*    Availability: https://codepen.io/fractalkitty/pen/bGjOZvj
+*
+***************************************************************************************/
+
+//click mouse to start
+//what you hear is the first draft of a song
+//another version is here: https://soundcloud.com/sophia-wood-977228849
+
+let five;
+let sArray = []; //array of dancing circles
+let spectrum;
+let waves;
+let fft;
+let t = 0;
+let n = 100;
+let timer = 5;
+let button;
+
+function preload() {
+  userStartAudio();
+  five = loadSound("FIVE.mp3"); //super rough piano doodle
+}
+function setup() {
+  sArray = [];
+  createCanvas(windowWidth, windowHeight);
+  five.amp(0.7); //how loud?
+  fft = new p5.FFT(); //creates sound array analyze
+  spectrum = fft.analyze();
+  for (let i = 0; i < spectrum.length; i++) {
+    sArray.push(new dots(i));
+  }
+  background(0, 0, 20);
+}
+
+function draw() {
+  translate(width / 2, height / 2);
+  //rotate(frameCount / 1000);
+
+  t += 0.001;
+  if (!five.isPlaying()) {
+    five.play();
+  }
+
+  background(0, 0, 9, 20);
+  soundProcess();
+
+  if (frameCount % 60 == 0 && timer > 0) {
+    // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+    timer--;
+  }
+  //  console.log(timer);
+  if (timer == 0) {
+    button = createButton("Carry on Listening");
+    button.position(200, height/2);
+    button.mousePressed(function goToAnotherPage() {
+      window.location.href =
+        "https://editor.p5js.org/natashatan/sketches/k-CVCaSDC";
+    });
+    button = createButton("Let's Breath");
+    button.position(425, height/2);
+    button.mousePressed(function goToAnotherPage() {
+      window.location.href =
+        "https://editor.p5js.org/natashatan/sketches/gxSMDJpDT";
+    });
+  }
+}
+
+function soundProcess() {
+  spectrum = fft.analyze();
+  waves = fft.waveform();
+  for (let i = 0; i < spectrum.length; i += 1) {
+    sArray[i].display(spectrum[i] / 10, waves[i]);
+  }
+}
+
+class dots {
+  constructor(i) {
+    this.i = i;
+    let c =
+      (min(width, height) - (min(width, height) / spectrum.length) * i) * 0.5;
+    this.L = random(-c, c);
+    this.theta = random(-PI, PI);
+    this.x = this.L * sin(this.theta);
+    this.y = this.L * cos(this.theta);
+    this.t = 1;
+    this.dt = random(0.00002);
+  }
+  display(dt, w) {
+    this.t += (10 * sin(this.dt + (abs(w / 100) + abs(dt / 10000)))) / 10;
+    this.theta = TWO_PI * sin(this.t);
+    this.x = this.L * sin(this.theta);
+    this.y = this.L * cos(this.theta);
+    fill(
+      125 * sin((PI * cos(this.t)) / 4),
+      355 * abs(sin(t / 2 + this.t)),
+      355 * abs(sin(t / 2 + this.t)),
+      50
+    );
+
+    if (this.i % 10 === 0) {
+      push();
+      fill(100, 200, 250, 1);
+      circle(
+        this.x * cos(this.t) + this.L * sin(this.t - this.L) * cos(t * this.dt),
+        this.y * sin(this.t + t / 2) +
+          this.L * cos(this.t + this.L) +
+          this.L * this.t * tan(t * this.dt),
+        this.x + this.L * sin(this.L / 100 + this.t)
+      );
+      pop();
+    }
+    circle(
+      this.x * cos(this.t),
+      this.y * sin(this.t + t / 2),
+      1 + 10 * abs(sin(w + dt / 100))
+    );
+    let f = width / 2;
+    noStroke();
+  }
+}
+function mousePressed() {
+  playBool = !playBool;
+}
+function keyPressed() {
+  five.stop();
+  setup();
+  draw();
+}
